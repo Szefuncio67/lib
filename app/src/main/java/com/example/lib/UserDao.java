5f851aed85c3175ca973066d4a9d2fb0fb6562b4
Szefuncio67 <mateusz8301@wp.pl>
2024-01-24 14:21:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lib/UserDao.java
@@ -0,0 +1,17 @@
+package com.example.lib;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+@Dao
+public interface UserDao {
+
+    @Insert
+    void registerUser(UserEntity userEntity);
+
+    @Query("SELECT * FROM users WHERE userId =(:userId) AND password =(:password)")
+    UserEntity login(String userId, String password);
+
+
+}
